c481f756a3376478a9bdd33d322355f3e5f76053
nysascape <nysadev@raphielgang.org>
2019-09-08 02:27:57 +0000

    platina: Move to common lunch choice
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,5 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/potato_platina.mk
+
+COMMON_LUNCH_CHOICES := \
+	potato_platina-userdebug
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo potato_platina-userdebug
-add_lunch_combo potato_platina-eng
-add_lunch_combo potato_platina-user
