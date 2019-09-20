f08bf143f8aee2e37e15bd73f009083370819d6b
nysascape <nysadev@raphielgang.org>
2019-09-20 12:30:51 +0000

    treewide: Changes for EvoX
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -17,19 +17,22 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit from platina device
 $(call inherit-product, device/xiaomi/platina/device.mk)
 
-# Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+export CUSTOM_BUILD_TYPE=OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES_EVO := true
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosip_platina
+PRODUCT_NAME := aosp_platina
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := MI 8 Lite
+PRODUCT_MODEL := Mi 8 Lite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
